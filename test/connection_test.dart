d03d5cb610adf2fe8ce71f7e56185d484673460a
Konstantin Wachendorff <kw@eyev.de>
2023-01-09 12:29:12 +0100

    comment out real connection test need to distinguish between real device and simulated

--- test/connection_test.dart
+++ test/connection_test.dart
@@ -9,36 +9,36 @@ import 'package:test/test.dart';
 
 // This test only runs with a real skyle connected.
 
-void main() {
-  final et = ET();
-  setUpAll(() async {});
+// void main() {
+//   final et = ET();
+//   setUpAll(() async {});
 
-  tearDownAll(() async {});
-  ConnectionTester.run(et);
-}
+//   tearDownAll(() async {});
+//   ConnectionTester.run(et);
+// }
 
-class ConnectionTester {
-  static void run(ET et) {
-    group('Test connect and disconnect scenarios', () {
-      test('Test hard-connect, get versions, hard-disconnect in a loop', () async {
-        for (final _ in List.generate(10, (i) => i)) {
-          await et.connect();
-          final versions = await et.versions.get();
-          if (versions is DataSuccess) print(versions.data!.serial);
-          await et.disconnect();
-        }
-      });
+// class ConnectionTester {
+//   static void run(ET et) {
+//     group('Test connect and disconnect scenarios', () {
+//       test('Test hard-connect, get versions, hard-disconnect in a loop', () async {
+//         for (final _ in List.generate(10, (i) => i)) {
+//           await et.connect();
+//           final versions = await et.versions.get();
+//           if (versions is DataSuccess) print(versions.data!.serial);
+//           await et.disconnect();
+//         }
+//       });
 
-      test('Test soft-connect, get versions, soft-disconnect in a loop', () async {
-        await et.connect();
-        for (final _ in List.generate(10, (i) => i)) {
-          await et.trySoftReconnect();
-          final versions = await et.versions.get();
-          if (versions is DataSuccess) print(versions.data!.serial);
-          await et.softDisconnect();
-        }
-        await et.disconnect();
-      });
-    });
-  }
-}
+//       test('Test soft-connect, get versions, soft-disconnect in a loop', () async {
+//         await et.connect();
+//         for (final _ in List.generate(10, (i) => i)) {
+//           await et.trySoftReconnect();
+//           final versions = await et.versions.get();
+//           if (versions is DataSuccess) print(versions.data!.serial);
+//           await et.softDisconnect();
+//         }
+//         await et.disconnect();
+//       });
+//     });
+//   }
+// }
